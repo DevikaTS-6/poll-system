08ed0f712ab1009eb746d09e76b2db25ce4d22fd
DevikaTS-6 <devikats765@gmail.com>
2025-06-20 00:49:26 +0530

    connect frontend to render backend

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -2,4 +2,4 @@
 import { io } from "socket.io-client";
 
 // Make sure this URL matches your backend (dev: localhost, prod: Render)
-export const socket = io("http://localhost:5000");
+export const socket = io("https://poll-system-server.onrender.com");
